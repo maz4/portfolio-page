1fe2c0703c6e36ab36b983124ceb318c97d6bb08
veler86 <zasadzkimarcin@gmail.com>
2018-09-09 13:42:44 +0100

    add scroll file to the js folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
-(function(portfolio){
+(function (portfolio) {
   'use strict';
-  
+
   var menuElem = document.querySelector('.nav-menu');
-  if(menuElem){
+  if (menuElem) {
     var menu = Object.create(portfolio.Menu);
     menu.init(menuElem);
   }
 
+  var linkElem = document.querySelectorAll('.nav-menu-link');
+  if (linkElem.length) {
+    for (var i = 0; i < linkElem.length; i++) {
+      var link = Object.create(portfolio.SmoothScroll);
+      link.init(linkElem[i]);
+    }
+  }
 }(window.portfolio || {}));
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,21 +1,21 @@
-(function(window){
+(function (window) {
   'use strict';
 
   window.portfolio = window.portfolio || {};
 
   window.portfolio.Menu = {
-    init: function(elem){
+    init: function (elem) {
       this.elem = elem;
       this.button = this.elem.querySelector('.nav-menu__toggle');
       this.elem.classList.add('nav-menu--close')
       this.toggleMenu();
     },
 
-    toggleMenu: function(){
+    toggleMenu: function () {
       this.button.addEventListener('click', this.onButtonClick.bind(this));
     },
 
-    onButtonClick: function(){
+    onButtonClick: function () {
       event.preventDefault();
       this.elem.classList.toggle('nav-menu--close');
     }
