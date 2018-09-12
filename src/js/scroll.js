0b8f4f5106cae06aa5afb90f89990564f406b861
veler86 <zasadzkimarcin@gmail.com>
2018-09-12 20:06:18 +0100

    pass event the onClick function, improve scrolling

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -8,45 +8,44 @@
       this.linkElem = linkElem;
       this.targetId = this.linkElem.getAttribute('href');
       this.target = document.querySelector(this.targetId);
-      this.targetPosition = this.target.getBoundingClientRect().top;
-      this.startPosition = window.pageYOffset;
-      this.distance = this.targetPosition - this.startPosition;
-      this.startTime = null;
       this.duration = 1000;
-      this.menuLink();
-    },
-
-    srcollSetup: function(){
-      this.startPosition = window.pageYOffset;
-      this.distance = this.targetPosition - this.startPosition;
-      this.startTime = null;
+      this.setEvents();
     },
 
     ease: function (timeE, startP, dist, durat) {
       timeE /= durat / 2;
-      if (timeE < 1) return dist / 2 * timeE * timeE + startP;
+      if (timeE < 1) {
+        return dist / 2 * timeE * timeE + startP;
+      }
       timeE--;
       return -dist / 2 * (timeE * (timeE - 2) - 1) + startP;
     },
 
-    animation: function (currentTime) {
-      if (this.startTime === null) this.startTime = currentTime;
+    animate: function (currentTime) {
+      if (this.startTime === null) {
+        this.startTime = currentTime;
+      }
+      
       var timeElapsed = currentTime - this.startTime;
       var run = this.ease(timeElapsed, this.startPosition, this.distance, this.duration);
       window.scrollTo(0, run);
-      if (timeElapsed < this.duration) requestAnimationFrame(this.animation.bind(this));
+
+      if (timeElapsed < this.duration) {
+        requestAnimationFrame(this.animate.bind(this));
+      }
     },
 
-    menuLink: function () {
+    setEvents: function () {
       this.linkElem.addEventListener('click', this.onClick.bind(this));
     },
 
-    onClick: function () {
-      console.log(this.startPosition, this.targetPosition);
+    onClick: function (event) {
       event.preventDefault();
-      this.srcollSetup();
-      console.log(this.startPosition, this.targetPosition);
-      requestAnimationFrame(this.animation.bind(this));
+      this.targetPosition = this.target.getBoundingClientRect().top;
+      this.startPosition = window.pageYOffset;
+      this.distance = this.targetPosition - this.startPosition;
+      this.startTime = null;
+      requestAnimationFrame(this.animate.bind(this));
     }
   }
 }(window));
